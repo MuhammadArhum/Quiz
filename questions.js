34e8e66638a3d3369cdabdee15d6105737b19dea
Muhammad Arhum Javed <78553500+MuhammadArhum@users.noreply.github.com>
2021-06-15 10:16:17 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -13,13 +13,13 @@ let questions = [
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "Most Corrupt politican?",
+    answer: "Nawaz Sharif",
     options: [
       "Common Style Sheet",
       "Colorful Style Sheet",
       "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "Nawaz Sharif"
     ]
   },
     {
